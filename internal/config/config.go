8e9b82461c625d2e473039869b2ecd8d5d7d108d
Alexander Teterin <alexander.teterin@ispringsolutions.com>
2026-03-13 18:30:27 +0300

    добавил получение конфигов из переменных окружения

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,9 @@
 package config
 
-import "flag"
+import (
+	"flag"
+	"os"
+)
 
 type Config struct {
 	AppAddr       string
@@ -13,6 +16,14 @@ func ParseFlags() Config {
 	flag.StringVar(&appAddr, "a", ":8080", "port to run server")
 	flag.StringVar(&resultBaseUrl, "b", "http://localhost:8080", "base url for short url")
 	flag.Parse()
+
+	if envAppAddr := os.Getenv("SERVER_ADDRESS"); envAppAddr != "" {
+		appAddr = envAppAddr
+	}
+	if envResultBaseUrl := os.Getenv("BASE_URL"); envResultBaseUrl != "" {
+		resultBaseUrl = envResultBaseUrl
+	}
+
 	return Config{
 		AppAddr:       appAddr,
 		ResultBaseUrl: resultBaseUrl,
